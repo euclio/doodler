e556f6d8fd3736d3aa729b5e39711bdef5cc1c4b
Andy Russell <arussell123@gmail.com>
2012-05-22 15:31:12 -0700

    Removed old directory structure

--- doodler/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        final DoodleGUI gui = new DoodleGUI("Doodler!");
-        gui.setVisible(true);
-    }
-}
--- doodler/src/Shape.java
+++
@@ -1,8 +0,0 @@
-public enum Shape {
-    CIRCLE, SQUARE, TRIANGLE;
-
-    public String toString() {
-	String word = name();
-	return word.charAt(0) + word.substring(1).toLowerCase();
-    }
-}
