489e13eafb269d4a955db5b905bdb1827d92960b
Andy Russell <arussell123@gmail.com>
2012-05-22 22:58:51 -0700

    Shapes can now return a Polygon representation of themselves with a given size at a given point.

--- src/Shape.java
+++ src/Shape.java
@@ -1,8 +1,33 @@
+import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+
 public enum Shape {
     CIRCLE, SQUARE, TRIANGLE;
 
     public String toString() {
-	String word = name();
-	return word.charAt(0) + word.substring(1).toLowerCase();
+        String word = name();
+        return word.charAt(0) + word.substring(1).toLowerCase();
+    }
+
+    public java.awt.Shape render(Point p, int size) {
+        switch (this) {
+        case SQUARE:
+            return new Rectangle2D.Float(p.x - size / 2, p.y - size / 2, size,
+                    size);
+
+        case CIRCLE:
+            return new Ellipse2D.Float(p.x - size / 2, p.y - size / 2, size,
+                    size);
+
+        case TRIANGLE:
+            int[] xPoints = { p.x - size / 2, p.x + size / 2, p.x };
+            int[] yPoints = { p.y + size / 2, p.y + size / 2, p.y - size / 2 };
+            return new Polygon(xPoints, yPoints, 3);
+
+        default:
+            return null;
+        }
     }
 }
