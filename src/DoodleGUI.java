e2fc04788d573820bdbc8ab7a1fb5882a4d4a1ec
Andy Russell <arussell123@gmail.com>
2012-05-22 22:56:54 -0700

    Added a pane to hold the new tools function, changed the default close behavior to ask if the current doodle should be saved, and fixed a bug if no color was selected after entering the color chooser

--- src/DoodleGUI.java
+++ src/DoodleGUI.java
@@ -1,5 +1,8 @@
-import java.awt.*; 
+import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.*;
 import javax.swing.event.*;
 
@@ -14,6 +17,8 @@ public class DoodleGUI extends JFrame {
     private static final int WINDOW_WIDTH = 1024;
     private static final int WINDOW_X = 50;
     private static final int WINDOW_Y = 50;
+    private static final int TOOL_H_GAP = 4;
+    private static final int TOOL_V_GAP = 2;
 
     public DoodleGUI(String title) {
         super(title);
@@ -21,13 +26,13 @@ public class DoodleGUI extends JFrame {
             // Set System L&F
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (UnsupportedLookAndFeelException e) {
-            // handle exception
+            System.err.println("ERROR: " + e.getMessage());
         } catch (ClassNotFoundException e) {
-            // handle exception
+            System.err.println("ERROR: " + e.getMessage());
         } catch (InstantiationException e) {
-            // handle exception
+            System.err.println("ERROR: " + e.getMessage());
         } catch (IllegalAccessException e) {
-            // handle exception
+            System.err.println("ERROR: " + e.getMessage());
         }
 
         SwingUtilities.invokeLater(new Runnable() {
@@ -42,9 +47,24 @@ public class DoodleGUI extends JFrame {
         // Initialize the main panes of the GUI
         final JPanel options = new JPanel();
         final Canvas canvas = new Canvas();
-        final JMenuBar menuBar = new DoodleMenuBar(this, canvas);
+        final DoodleMenuBar menuBar = new DoodleMenuBar(canvas);
 
         // Add the panels within the options pane
+        // Create the Tools Pane
+        JPanel tools = new JPanel();
+        tools.setBorder(BorderFactory.createTitledBorder("Tools"));
+        tools.setLayout(new GridLayout(2, Tools.values().length / 2, TOOL_H_GAP, TOOL_V_GAP));
+        ButtonGroup toolsGroup = new ButtonGroup();
+        
+        
+        // Enumerate through all tools
+        for (Tools t : Tools.values()) {
+            tools.add(t.getButton());
+            toolsGroup.add(t.getButton());
+        }
+        
+        options.add(tools);
+
         // Initialize the size panel
         JPanel size = new JPanel();
         size.setBorder(BorderFactory.createTitledBorder("Size"));
@@ -94,8 +114,10 @@ public class DoodleGUI extends JFrame {
             public void mousePressed(MouseEvent e) {
                 Color newColor = JColorChooser.showDialog(colorChooser,
                         "Choose Color", canvas.getColor());
-                canvas.setColor(newColor);
-                colorLabel.setBackground(newColor);
+                if (newColor != null) {
+                    canvas.setColor(newColor);
+                    colorLabel.setBackground(newColor);
+                }
             }
         });
 
@@ -107,14 +129,13 @@ public class DoodleGUI extends JFrame {
         JPanel shapePanel = new JPanel();
         shapePanel.setBorder(BorderFactory.createTitledBorder("Shape"));
 
-        JComboBox/*<Shape>*/ shapes = new JComboBox/*<Shape>*/(Shape.values());
+        JComboBox shapes = new JComboBox(Shape.values());
 
         // Class to listen for change of shapes
         shapes.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // @SuppressWarnings("unchecked")
-                JComboBox/*<Shape>*/ cb = (JComboBox/*<Shape>*/)e.getSource();
+                JComboBox cb = (JComboBox) e.getSource();
                 canvas.setShape((Shape) cb.getSelectedItem());
             }
         });
@@ -134,7 +155,13 @@ public class DoodleGUI extends JFrame {
 
         // Final settings for the main GUI
         this.setLocation(WINDOW_X, WINDOW_Y);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                menuBar.exit();
+            }
+        });
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         this.setMinimumSize(this.getLayout().minimumLayoutSize(this));
         this.setVisible(true);
