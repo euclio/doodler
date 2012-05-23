438633aa7cfdf0e6d26269ffe22ded01f84fe21f
Andy Russell <arussell123@gmail.com>
2012-05-22 22:57:58 -0700

    Made the DoodleMenuBar non-static and fixed some messy parameter passing.  Added a "Save As" option and fixed some rare glitches when saving a file.

--- src/DoodleMenuBar.java
+++ src/DoodleMenuBar.java
@@ -1,3 +1,4 @@
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -7,7 +8,6 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -16,11 +16,11 @@ import javax.swing.JOptionPane;
 @SuppressWarnings("serial")
 public class DoodleMenuBar extends JMenuBar {
     public enum MenuChoices {
-        // Help
-        ABOUT, EXIT, // File
-        NEW, OPEN,
+        // File
+        NEW, OPEN, SAVE, SAVEAS, EXIT,
 
-        SAVE
+        // Help
+        ABOUT
     };
 
     class MenuListener implements ActionListener {
@@ -31,32 +31,35 @@ public class DoodleMenuBar extends JMenuBar {
 
             switch (MenuChoices.valueOf(itemText.toUpperCase())) {
             case NEW:
-                newFile(gui, canvas);
+                newFile();
                 break;
             case OPEN:
-                openFile(gui, canvas);
+                openFile();
                 break;
             case SAVE:
-                saveFile(gui, canvas);
+                saveFile();
+                break;
+            case SAVEAS:
+                saveAs();
                 break;
             case EXIT:
-                exit(gui, canvas);
+                exit();
                 break;
 
             // Help Menu
             case ABOUT:
-                about(gui, canvas);
+                about();
             }
         }
     }
 
+    private final Container rootPane = this.getTopLevelAncestor();
     private final Canvas canvas;
     private static final String VALID_IMAGES = "([^\\s]+(\\.(?i)(jpg|png|gif|bmp))$)";
-    private final DoodleGUI gui;
-
-    public DoodleMenuBar(DoodleGUI gui, Canvas canvas) {
+    
+    
+    public DoodleMenuBar(Canvas canvas) {
         super();
-        this.gui = gui;
         this.canvas = canvas;
         // Class to handle menu events
 
@@ -76,6 +79,10 @@ public class DoodleMenuBar extends JMenuBar {
         JMenuItem saveFile = new JMenuItem("Save...");
         saveFile.addActionListener(menuListener);
         fileMenu.add(saveFile);
+        
+        JMenuItem saveAs = new JMenuItem("Save As...");
+        saveAs.addActionListener(menuListener);
+        fileMenu.add(saveAs);
 
         fileMenu.addSeparator();
 
@@ -95,38 +102,36 @@ public class DoodleMenuBar extends JMenuBar {
         this.add(helpMenu);
     }
 
-    public static void about(JFrame f, Canvas canvas) {
+    public void about() {
         JOptionPane
                 .showMessageDialog(
-                        f,
+                        rootPane,
                         "Doodler is a simple drawing program written in Java using Swing/AWT.  Coded with love by Andy Russell.",
                         "About Doodler", JOptionPane.PLAIN_MESSAGE);
     }
 
-    public static void exit(JFrame f, Canvas canvas) {
+    public void exit() {
         int result = 0;
         if (canvas.isModified()) {
             result = JOptionPane
                     .showConfirmDialog(
-                            f,
-                            "The current doodle is not yet saved.  Would you like to save?",
+                            rootPane,
+                            "The current doodle is not saved.  Would you like to save it?",
                             "Warning", JOptionPane.YES_NO_CANCEL_OPTION);
-        }
+            if (result == JOptionPane.YES_OPTION) {
+                saveFile();
+            }
 
-        if (result == JOptionPane.YES_OPTION) {
-            canvas.saveImage();
+            if (result == JOptionPane.CANCEL_OPTION) {
+                return;
+            }
         }
-
-        if (result == JOptionPane.CANCEL_OPTION) {
-            return;
-        }
-
         System.exit(0);
     }
 
-    public static void newFile(JFrame f, Canvas canvas) {
+    public void newFile() {
         if (canvas.isModified()) {
-            int choice = JOptionPane.showConfirmDialog(f,
+            int choice = JOptionPane.showConfirmDialog(rootPane,
                     "Are you sure you want to start over?", "Confirm Reset",
                     JOptionPane.YES_NO_OPTION);
             if (choice == JOptionPane.YES_OPTION)
@@ -136,18 +141,18 @@ public class DoodleMenuBar extends JMenuBar {
         }
     }
 
-    public static void openFile(JFrame f, Canvas canvas) {
+    public void openFile() {
         // OPEN
         int result;
 
         if (canvas.isModified()) {
             result = JOptionPane
                     .showConfirmDialog(
-                            f,
-                            "The current doodle is not yet saved.  Would you like to save?",
+                            rootPane,
+                            "The current doodle is not saved.  Would you like to save it?",
                             "Warning", JOptionPane.YES_NO_CANCEL_OPTION);
             if (result == JOptionPane.YES_OPTION) {
-                canvas.saveImage();
+                saveFile();
             }
 
             if (result == JOptionPane.CANCEL_OPTION) {
@@ -155,8 +160,8 @@ public class DoodleMenuBar extends JMenuBar {
             }
         }
 
-        final JFileChooser chooser = new JFileChooser();
-        int returnVal = chooser.showOpenDialog(f);
+        final JFileChooser chooser = new JFileChooser(Canvas.DEFAULT_SAVE_DIR);
+        int returnVal = chooser.showOpenDialog(rootPane);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             File newFile = chooser.getSelectedFile();
             try {
@@ -167,14 +172,42 @@ public class DoodleMenuBar extends JMenuBar {
                     throw new IOException("This file is not an image.");
                 }
             } catch (IOException exc) {
-                JOptionPane.showMessageDialog(f, exc.getMessage(),
+                JOptionPane.showMessageDialog(rootPane, exc.getMessage(),
                         "Error When Opening File", JOptionPane.ERROR_MESSAGE);
 
             }
         }
     }
 
-    public static void saveFile(JFrame f, Canvas canvas) {
-        canvas.saveImage();
+    public void saveAs() {
+        JFileChooser chooser = new JFileChooser(Canvas.DEFAULT_SAVE_DIR);
+        chooser.setSelectedFile(canvas.getCurrentFile());
+        int returnVal = chooser.showSaveDialog(rootPane);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            if (chooser.getSelectedFile().exists()) {
+                returnVal = JOptionPane.showConfirmDialog(rootPane, chooser.getSelectedFile().getName() + " already exists.  Overwrite?", "Overwrite File?", JOptionPane.YES_NO_OPTION);
+                if (returnVal == JOptionPane.NO_OPTION) {
+                    return;
+                }
+            }
+            canvas.setCurrentFile(chooser.getSelectedFile());
+            canvas.setSaved(true);
+            saveFile();
+        }
+    }
+
+    public void saveFile() {
+        if (canvas.isSaved()) {
+            try {
+                ImageIO.write(canvas.getImage(), "png", canvas.getCurrentFile());
+                canvas.setModified(false);
+            } catch (IOException exc) {
+                System.err.println("Exception thrown during write");
+                // Handle exception
+            }
+        } else {
+            saveAs();
+        }
+        return;
     }
 }
